29ece2a3b910e3651323b71b04ad530cc7eeec25
cyrus <56029500+jancyrusm@users.noreply.github.com>
2025-02-18 10:21:13 +0800

    added side drawer

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:meals/main.dart';
 import 'package:meals/data/dummy_data.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 import 'package:meals/screens/meals.dart';
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -68,12 +68,10 @@ class MealItem extends StatelessWidget {
                             icon: Icons.schedule,
                             label: '${meal.duration} min'),
                         const SizedBox(width: 12),
-                        MealItemTrait(
-                            icon: Icons.work, label: '$complexityText'),
+                        MealItemTrait(icon: Icons.work, label: complexityText),
                         const SizedBox(width: 12),
                         MealItemTrait(
-                            icon: Icons.attach_money,
-                            label: '$affordabilityText'),
+                            icon: Icons.attach_money, label: affordabilityText),
                       ],
                     )
                   ],
